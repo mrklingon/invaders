0641ca335b0597b63e098904721cde4c2a2f2f76
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-12-28 16:17:58 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -30,6 +30,7 @@ input.onButtonPressed(Button.AB, function () {
         basic.pause(100)
     }
     Torpedo.delete()
+    game.addScore(Enemy[Player.get(LedSpriteProperty.X)] * 50)
     Enemy[Player.get(LedSpriteProperty.X)] = zero
 })
 input.onButtonPressed(Button.B, function () {
@@ -69,7 +70,7 @@ let Player: game.LedSprite = null
 let Enemy: number[] = []
 let zero = 0
 zero = 0
-game.setLife(5)
+game.setLife(10)
 Enemy = [1, 1, 1, 1, 1]
 let Blank = [0, 0, 0, 0, 0]
 Player = game.createSprite(2, 4)
@@ -80,6 +81,9 @@ basic.forever(function () {
             DispEnemy(index)
             basic.pause(2000)
             DispBlank(index)
+        }
+        for (let index = 0; index <= 4; index++) {
+            game.removeLife(Enemy[index])
         }
         Enemy = [1, 1, 1, 1, 1]
     }
